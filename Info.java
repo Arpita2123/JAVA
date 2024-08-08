2cc4e6f2ba86bd1660defe126e24b241767fb7c0
Arpita Taware <159871699+Arpita2123@users.noreply.github.com>
2024-08-07 22:49:58 -0700

    Add files via upload

---
+++ Info.java
@@ -0,0 +1,17 @@
+class Info
+{
+	public static void main(String args[])
+	{
+		System.out.println("Name : XYZ");
+		System.out.println("Adress : Pune");
+		System.out.println("Age : 21");
+		System.out.println("Mob. No : xxxxxxx300");
+		System.out.println("Mail : xyz000@gmail.com");
+		System.out.println("Class :SY_B.tech");
+		System.out.println("Course : CSE_AI");
+		System.out.println("CGPA: 8.80");
+
+
+	}
+
+}
