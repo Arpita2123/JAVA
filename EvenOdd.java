a78d9a46b008a879bf8d3f51c35f28b17050d669
Arpita Taware <159871699+Arpita2123@users.noreply.github.com>
2024-08-07 22:49:44 -0700

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,22 @@
+class EvenOdd
+{
+	public static void main(String args[])
+	{
+		int arr[]={1,2,3,4,5,6,7,8,9};
+		int i;
+		for(i=0;i<8;i++)
+		{
+			if(arr[i]%2==0)
+			{
+				System.out.println(arr[i] +"--Even");
+				
+			}
+			else
+			{
+				System.out.println(arr[i]+"--Odd");
+
+			}
+		}
+	}
+
+}
